65da088b1d9444d1ad3279f47a09ef91c7598a0e
glocalme1 <glocalmetest@gmail.com>
2015-10-14 22:36:02 +0800

    Initial Version

---
+++ src/main/java/com/glocalme/spring/ServiceConfig.java
@@ -0,0 +1,12 @@
+package com.glocalme.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.glocalme.service","com.mycompany.myproject.service"})
+public class ServiceConfig {
+   
+     
+
+}
